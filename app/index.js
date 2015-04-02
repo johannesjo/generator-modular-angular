060b107a615b3286d0f1e11b428557402682b317
Johannes Millan <johannes.millan@gmail.com>
2015-04-02 21:53:54 +0200

    more beautiful syntax for main generator

--- app/index.js
+++ app/index.js
@@ -4,252 +4,252 @@ var path = require('path');
 var util = require('util');
 var angularUtils = require('../util.js');
 var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
 var wiredep = require('wiredep');
 var chalk = require('chalk');
 
-var Generator = module.exports = function Generator(args, options)
-{
-    yeoman.generators.Base.apply(this, arguments);
-
-    // get app name
-    this.argument('appname', {type: String, required: false});
-    this.appname = this.appname || path.basename(process.cwd());
-    this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
-    this.slugAppName = this._.slugify(this.appname);
-    this.humanizedAppName = this._.humanize(this.appname);
-    this.scriptAppName = this.appname;
-
-
-    // get app path
-    try {
-        this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
-    } catch (e) {
-    }
-    this.env.options.appPath = this.env.options.appPath || 'app';
-    this.options.appPath = this.env.options.appPath;
-    this.appPath = this.env.options.appPath;
-
-
-    this.pkg = require('../package.json');
-    this.sourceRoot(path.join(__dirname, '../templates'));
-};
-
-util.inherits(Generator, yeoman.generators.Base);
-
-Generator.prototype.askForModules = function askForModules()
-{
-    var cb = this.async();
-
-    var prompts = [{
-        type: 'checkbox',
-        name: 'modules',
-        message: 'Which modules would you like to include?',
-        choices: [
-            {
-                value: 'animateModule',
-                name: 'angular-animate.js',
-                checked: true
-            }, {
-                value: 'ariaModule',
-                name: 'angular-aria.js',
-                checked: true
-            }, {
-                value: 'cookiesModule',
-                name: 'angular-cookies.js',
-                checked: false
-            }, {
-                value: 'resourceModule',
-                name: 'angular-resource.js',
-                checked: true
-            }, {
-                value: 'sanitizeModule',
-                name: 'angular-sanitize.js',
-                checked: false
-            }, {
-                value: 'touchModule',
-                name: 'angular-touch.js',
-                checked: false
-            }, {
-                value: 'ngMaterialModule',
-                name: 'angular material design',
-                checked: false
-            }, {
-                value: 'ngFabFormModule',
-                name: 'ng-fab-form (form helper)',
-                checked: true
-            }, {
-                value: 'messagesModule',
-                name: 'angular-messages.js (included with ngFabForm)',
-                checked: false
-            }, {
-                value: 'uiRouterModule',
-                name: 'ui-router.js',
-                checked: true
-            }, {
-                value: 'routeModule',
-                name: 'angular-route.js (standard router dot\'t use with ui-router)',
-                checked: false
-            }
-        ]
-    }];
-
-    this.prompt(prompts, function (props)
+module.exports = yeoman.generators.Base.extend({
+    constructor: function ()
     {
-        var hasMod = function (mod)
+        yeoman.generators.Base.apply(this, arguments);
+
+        // get app name
+        this.argument('appname', {type: String, required: false});
+        this.appname = this.appname || path.basename(process.cwd());
+        this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
+        this.slugAppName = this._.slugify(this.appname);
+        this.humanizedAppName = this._.humanize(this.appname);
+        this.scriptAppName = this.appname;
+
+
+        // get app path
+        try {
+            this.env.options.appPath = require(path.join(process.cwd(), 'bower.json')).appPath;
+        } catch (e) {
+        }
+        this.env.options.appPath = this.env.options.appPath || 'app';
+        this.options.appPath = this.env.options.appPath;
+        this.appPath = this.env.options.appPath;
+
+
+        this.pkg = require('../package.json');
+        this.sourceRoot(path.join(__dirname, '../templates'));
+    },
+
+    askForModules: function askForModules()
+    {
+        var cb = this.async();
+
+        var prompts = [{
+            type: 'checkbox',
+            name: 'modules',
+            message: 'Which modules would you like to include?',
+            choices: [
+                {
+                    value: 'animateModule',
+                    name: 'angular-animate.js',
+                    checked: true
+                }, {
+                    value: 'ariaModule',
+                    name: 'angular-aria.js',
+                    checked: true
+                }, {
+                    value: 'cookiesModule',
+                    name: 'angular-cookies.js',
+                    checked: false
+                }, {
+                    value: 'resourceModule',
+                    name: 'angular-resource.js',
+                    checked: true
+                }, {
+                    value: 'sanitizeModule',
+                    name: 'angular-sanitize.js',
+                    checked: false
+                }, {
+                    value: 'touchModule',
+                    name: 'angular-touch.js',
+                    checked: false
+                }, {
+                    value: 'ngMaterialModule',
+                    name: 'angular material design',
+                    checked: false
+                }, {
+                    value: 'ngFabFormModule',
+                    name: 'ng-fab-form (form helper)',
+                    checked: true
+                }, {
+                    value: 'messagesModule',
+                    name: 'angular-messages.js (included with ngFabForm)',
+                    checked: false
+                }, {
+                    value: 'uiRouterModule',
+                    name: 'ui-router.js',
+                    checked: true
+                }, {
+                    value: 'routeModule',
+                    name: 'angular-route.js (standard router dot\'t use with ui-router)',
+                    checked: false
+                }
+            ]
+        }];
+
+        this.prompt(prompts, function (props)
         {
-            return props.modules.indexOf(mod) !== -1;
-        };
-        this.animateModule = hasMod('animateModule');
-        this.ariaModule = hasMod('ariaModule');
-        this.cookiesModule = hasMod('cookiesModule');
-        this.messagesModule = hasMod('messagesModule');
-        this.resourceModule = hasMod('resourceModule');
-        this.routeModule = hasMod('routeModule');
-        this.uiRouterModule = hasMod('uiRouterModule');
-        this.ngFabFormModule = hasMod('ngFabFormModule');
-        this.sanitizeModule = hasMod('sanitizeModule');
-        this.ngMaterialModule = hasMod('ngMaterialModule');
-        this.touchModule = hasMod('touchModule');
-
-        var angMods = [];
-
-        if (this.animateModule) {
-            angMods.push("'ngAnimate'");
-        }
-
-        if (this.ariaModule) {
-            angMods.push("'ngAria'");
-        }
-
-        if (this.cookiesModule) {
-            angMods.push("'ngCookies'");
-        }
-
-        if (this.messagesModule) {
-            angMods.push("'ngMessages'");
-        }
-
-        if (this.resourceModule) {
-            angMods.push("'ngResource'");
-        }
-
-        if (this.routeModule) {
-            angMods.push("'ngRoute'");
-            this.env.options.ngRoute = true;
-        }
-
-        if (this.sanitizeModule) {
-            angMods.push("'ngSanitize'");
-        }
-
-        if (this.touchModule) {
-            angMods.push("'ngTouch'");
-        }
-
-        if (this.uiRouterModule) {
-            angMods.push("'ui.router'");
-            this.env.options.uiRouter = true;
-        }
-
-        if (this.ngMaterialModule) {
-            angMods.push("'ngMaterial'");
-            this.env.options.ngMaterial = true;
-        }
-
-        if (this.ngFabFormModule) {
-            angMods.push("'ngFabForm'");
-        }
-
-        if (angMods.length) {
-            this.env.options.angularDeps = '\n    ' + angMods.join(',\n    ') + '\n  ';
-        }
-
-        cb();
-    }.bind(this));
-};
-
-Generator.prototype.readIndex = function readIndex()
-{
-    this.ngRoute = this.env.options.ngRoute;
-    this.uiRouter = this.env.options.uiRouter;
-    this.ngMaterial = this.env.options.ngMaterial;
-    this.indexFile = this.engine(this.read('index.html'), this);
-};
-
-Generator.prototype.cssFiles = function bootstrapFiles()
-{
-    this.fs.copy(
-        this.templatePath('styles/**/*'),
-        this.destinationPath(path.join(this.appPath, 'styles/')
-        )
-    );
-};
-
-Generator.prototype.createIndexHtml = function createIndexHtml()
-{
-    this.indexFile = this.indexFile.replace(/&apos;/g, "'");
-    this.write(path.join(this.appPath, 'index.html'), this.indexFile);
-};
-
-Generator.prototype.appJs = function appJs()
-{
-    this.angularModules = this.env.options.angularDeps;
-
-    this.template('app/_app.js', 'app/scripts/_app.js');
-    this.template('app/_app.spec.js', 'app/scripts/_app.spec.js');
-    if (this.env.options.uiRouter) {
-        this.template('app/_routes.js', 'app/scripts/_routes.js');
-        this.template('app/_routes.spec.js', 'app/scripts/_routes.spec.js');
-    }
-};
-
-Generator.prototype.packageFiles = function packageFiles()
-{
-    this.template('root/_bower.json', 'bower.json');
-    this.template('root/_bowerrc', '.bowerrc');
-    this.template('root/_package.json', 'package.json');
-    this.template('root/_gulpfile.js', 'gulpfile.js');
-    this.template('root/_config.xml', 'config.xml');
-    this.template('root/_gitignore', '.gitignore');
-    this.template('root/_editorconfig', '.editorconfig');
-    this.template('root/_jshintrc', '.jshintrc');
-    this.template('root/README.md', 'README.md');
-    this.template('root/_travis.yml', '.travis.yml');
-    this.template('root/_karma.conf.js', 'karma.conf.js');
-    this.template('root/_karma-e2e.conf.js', 'karma-e2e.conf.js');
-};
-
-Generator.prototype.tasks = function packageFiles()
-{
-    this.template('tasks/config.js', 'tasks/config.js');
-    this.template('tasks/build.js', 'tasks/build.js');
-    this.template('tasks/dev.js', 'tasks/dev.js');
-    this.template('tasks/deploy.js', 'tasks/deploy.js');
-
-    // TODO make cordova optional
-    this.template('tasks/cordova.js', 'tasks/cordova.js');
-};
-
-Generator.prototype.installDeps = function packageFiles()
-{
-    this.on('end', function ()
-    {
-        // save configuration
-        this.config.save();
-
-        this.installDependencies({
-            callback: function ()
+            var hasMod = function (mod)
             {
-                // Emit a new event - dependencies installed
-                this.emit('dependenciesInstalled');
-            }.bind(this)
-        });
-    });
+                return props.modules.indexOf(mod) !== -1;
+            };
+            this.animateModule = hasMod('animateModule');
+            this.ariaModule = hasMod('ariaModule');
+            this.cookiesModule = hasMod('cookiesModule');
+            this.messagesModule = hasMod('messagesModule');
+            this.resourceModule = hasMod('resourceModule');
+            this.routeModule = hasMod('routeModule');
+            this.uiRouterModule = hasMod('uiRouterModule');
+            this.ngFabFormModule = hasMod('ngFabFormModule');
+            this.sanitizeModule = hasMod('sanitizeModule');
+            this.ngMaterialModule = hasMod('ngMaterialModule');
+            this.touchModule = hasMod('touchModule');
 
-    // Now you can bind to the dependencies installed event
-    this.on('dependenciesInstalled', function ()
+            var angMods = [];
+
+            if (this.animateModule) {
+                angMods.push("'ngAnimate'");
+            }
+
+            if (this.ariaModule) {
+                angMods.push("'ngAria'");
+            }
+
+            if (this.cookiesModule) {
+                angMods.push("'ngCookies'");
+            }
+
+            if (this.messagesModule) {
+                angMods.push("'ngMessages'");
+            }
+
+            if (this.resourceModule) {
+                angMods.push("'ngResource'");
+            }
+
+            if (this.routeModule) {
+                angMods.push("'ngRoute'");
+                this.env.options.ngRoute = true;
+            }
+
+            if (this.sanitizeModule) {
+                angMods.push("'ngSanitize'");
+            }
+
+            if (this.touchModule) {
+                angMods.push("'ngTouch'");
+            }
+
+            if (this.uiRouterModule) {
+                angMods.push("'ui.router'");
+                this.env.options.uiRouter = true;
+            }
+
+            if (this.ngMaterialModule) {
+                angMods.push("'ngMaterial'");
+                this.env.options.ngMaterial = true;
+            }
+
+            if (this.ngFabFormModule) {
+                angMods.push("'ngFabForm'");
+            }
+
+            if (angMods.length) {
+                this.env.options.angularDeps = '\n    ' + angMods.join(',\n    ') + '\n  ';
+            }
+
+            cb();
+        }.bind(this));
+    },
+
+    readIndex: function readIndex()
     {
-        this.spawnCommand('gulp', ['serve']);
-    });
-};
+        this.ngRoute = this.env.options.ngRoute;
+        this.uiRouter = this.env.options.uiRouter;
+        this.ngMaterial = this.env.options.ngMaterial;
+        this.indexFile = this.engine(this.read('index.html'), this);
+    },
+
+    cssFiles: function bootstrapFiles()
+    {
+        this.fs.copy(
+            this.templatePath('styles/**/*'),
+            this.destinationPath(path.join(this.appPath, 'styles/')
+            )
+        );
+    },
+
+    createIndexHtml: function createIndexHtml()
+    {
+        this.indexFile = this.indexFile.replace(/&apos;/g, "'");
+        this.write(path.join(this.appPath, 'index.html'), this.indexFile);
+    },
+
+    appJs: function appJs()
+    {
+        this.angularModules = this.env.options.angularDeps;
+
+        this.template('app/_app.js', 'app/scripts/_app.js');
+        this.template('app/_app.spec.js', 'app/scripts/_app.spec.js');
+        if (this.env.options.uiRouter) {
+            this.template('app/_routes.js', 'app/scripts/_routes.js');
+            this.template('app/_routes.spec.js', 'app/scripts/_routes.spec.js');
+        }
+    },
+
+    packageFiles: function packageFiles()
+    {
+        this.template('root/_bower.json', 'bower.json');
+        this.template('root/_bowerrc', '.bowerrc');
+        this.template('root/_package.json', 'package.json');
+        this.template('root/_gulpfile.js', 'gulpfile.js');
+        this.template('root/_config.xml', 'config.xml');
+        this.template('root/_gitignore', '.gitignore');
+        this.template('root/_editorconfig', '.editorconfig');
+        this.template('root/_jshintrc', '.jshintrc');
+        this.template('root/README.md', 'README.md');
+        this.template('root/_travis.yml', '.travis.yml');
+        this.template('root/_karma.conf.js', 'karma.conf.js');
+        this.template('root/_karma-e2e.conf.js', 'karma-e2e.conf.js');
+    },
+
+    tasks: function packageFiles()
+    {
+        this.template('tasks/config.js', 'tasks/config.js');
+        this.template('tasks/build.js', 'tasks/build.js');
+        this.template('tasks/dev.js', 'tasks/dev.js');
+        this.template('tasks/deploy.js', 'tasks/deploy.js');
+
+        // TODO make cordova optional
+        this.template('tasks/cordova.js', 'tasks/cordova.js');
+    },
+
+    installDeps: function packageFiles()
+    {
+        this.on('end', function ()
+        {
+            // save configuration
+            this.config.save();
+
+            this.installDependencies({
+                callback: function ()
+                {
+                    // Emit a new event - dependencies installed
+                    this.emit('dependenciesInstalled');
+                }.bind(this)
+            });
+        });
+
+        // Now you can bind to the dependencies installed event
+        this.on('dependenciesInstalled', function ()
+        {
+            this.spawnCommand('gulp', ['serve']);
+        });
+    }
+});
+
