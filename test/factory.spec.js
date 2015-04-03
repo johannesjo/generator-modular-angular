6376b6cc21089b67bef43288a70d3c6fd0df8cd4
Johannes Millan <johannes.millan@gmail.com>
2015-04-03 18:52:14 +0200

    added test for factory

--- test/factory.spec.js
+++ test/factory.spec.js
@@ -4,35 +4,35 @@ var assert = require('assert');
 var helpers = require('yeoman-generator').test;
 var assert = require('yeoman-generator').assert;
 
-describe('moda Service generator', function ()
+describe('moda Factory generator', function ()
 {
     // not testing the actual run of generators yet
     it('can be required without throwing', function ()
     {
-        this.app = require('../s');
+        this.app = require('../f');
     });
 
     describe('basic file creation', function ()
     {
         var testArguments = 'test-name';
         var expectedContent = [
-            ['app/scripts/_main/global-services/test-name-s.js', /TestName/],
-            ['app/scripts/_main/global-services/test-name-s.js', /module\('tmp'\)/],
-            ['app/scripts/_main/global-services/test-name-s.spec.js', /TestName/]
+            ['app/scripts/_main/global-services/test-name-f.js', /TestName/],
+            ['app/scripts/_main/global-services/test-name-f.js', /module\('tmp'\)/],
+            ['app/scripts/_main/global-services/test-name-f.spec.js', /TestName/]
         ];
         var expected = [
-            'app/scripts/_main/global-services/test-name-s.js',
-            'app/scripts/_main/global-services/test-name-s.spec.js'
+            'app/scripts/_main/global-services/test-name-f.js',
+            'app/scripts/_main/global-services/test-name-f.spec.js'
         ];
 
         var noFile = [
-            'app/scripts/test-name/test-name-s.js',
-            'app/scripts/test-name-s.js',
-            'app/scripts/global-services/test-name-f.js'
+            'app/scripts/test-name/test-name-f.js',
+            'app/scripts/test-name-f.js',
+            'app/scripts/global-services/test-name-s.js'
         ];
 
         var nonExpected = [
-            ['app/scripts/_main/global-services/test-name-s.js', /testName/]
+            ['app/scripts/_main/global-services/test-name-f.js', /testName/]
         ];
 
         var options = {
@@ -44,7 +44,7 @@ describe('moda Service generator', function ()
         beforeEach(function ()
         {
             runGen = helpers
-                .run(path.join(__dirname, '../s'))
+                .run(path.join(__dirname, '../f'))
                 .inDir(path.join(__dirname, '.tmp'))
         });
 
@@ -72,28 +72,28 @@ describe('moda Service generator', function ()
     });
 
 
-    describe('files in sub folder creation', function ()
+    describe('file in sub folder creation', function ()
     {
         var testArguments = 'test-name test-path';
         var expectedContent = [
-            ['app/scripts/test-path/test-name-s.js', /TestName/],
-            ['app/scripts/test-path/test-name-s.js', /module\('tmp'\)/],
-            ['app/scripts/test-path/test-name-s.spec.js', /TestName/]
+            ['app/scripts/test-path/test-name-f.js', /TestName/],
+            ['app/scripts/test-path/test-name-f.js', /module\('tmp'\)/],
+            ['app/scripts/test-path/test-name-f.spec.js', /TestName/]
         ];
         var expected = [
-            'app/scripts/test-path/test-name-s.js',
-            'app/scripts/test-path/test-name-s.spec.js'
+            'app/scripts/test-path/test-name-f.js',
+            'app/scripts/test-path/test-name-f.spec.js'
 
         ];
         var noFile = [
-            'app/scripts/test-path/test-name-f.js',
-            'app/scripts/test-name-s.js',
+            'app/scripts/test-path/test-name-s.js',
+            'app/scripts/test-name-f.js',
             'app/scripts/test-path/test-name-d.html',
             'app/scripts/test-path/_test-name-d.scss'
         ];
 
         var nonExpected = [
-            ['app/scripts/test-path/test-name-s.js', /testName/]
+            ['app/scripts/test-path/test-name-f.js', /testName/]
         ];
 
         var options = {
@@ -105,7 +105,7 @@ describe('moda Service generator', function ()
         beforeEach(function ()
         {
             runGen = helpers
-                .run(path.join(__dirname, '../s'))
+                .run(path.join(__dirname, '../f'))
                 .inDir(path.join(__dirname, '.tmp'))
         });
 
