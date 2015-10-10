a15c879ed85018e00728f18e60a9e961d196aeb0
Johannes Millan <johannes.millan@gmail.com>
2015-10-10 16:20:37 +0200

    updated directive templates

--- templates/app/directive.js
+++ templates/app/directive.js
@@ -7,22 +7,22 @@
 * # <%= cameledName %><%= nameSuffix %>
 */
 angular.module('<%= scriptAppName %>')
-.directive('<%= cameledName %><%= nameSuffix %>', function ()
-{
-    return {<% if(tplUrl) {%>
-        templateUrl: '<%= tplUrl %>',
-        <% } %>
-        restrict: 'EA',
-        scope: {
+    .directive('<%= cameledName %><%= nameSuffix %>', function ()
+    {
+        return {<% if(tplUrl) {%>
+            templateUrl: '<%= tplUrl %>',
+            <% } %>
+            restrict: 'EA',
+            scope: {
 
-        },
-        link: function (scope, el, attrs)
-        {
+            },
+            link: function (scope, el, attrs)
+            {
 
-        },
-        controller: function ($scope<% if(createService) {%>, <%= svcName %><% } %>)
-        {
+            },
+            controller: function ($scope<% if(createService) {%>, <%= svcName %><% } %>)
+            {
 
-        }
-    };
-});
+            }
+        };
+    });
