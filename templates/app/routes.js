fc80e788da5894e623f1f2e2a63f084eaa975aff
Johannes Millan <johannes.millan@gmail.com>
2016-02-19 20:08:08 +0000

    add welcome state home for better experiance for first time users

--- templates/app/routes.js
+++ templates/app/routes.js
@@ -20,6 +20,10 @@
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
+            .state('home', {
+                url: '/',
+                template: '<div><h1>Hello!</h1><p>All clean here, as intended! Why not build some routes via "yo moda:r my-route"?</p></div>'
+            })
             /* STATES-NEEDLE - DO NOT REMOVE THIS */;
     }
 })();
